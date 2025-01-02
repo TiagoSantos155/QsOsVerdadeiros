20dde4cc47c8f5b5f5da9801602383a7d4f70ddc
TiagoSantos <47723@alunos.upt.pt>
2025-01-02 10:23:35 +0000

    Começo
    Um novo começo

--- TesteJavaFXMaven/src/main/java/org/example/testejavafxmaven/TestDatabaseConnection.java
+++ TesteJavaFXMaven/src/main/java/org/example/testejavafxmaven/TestDatabaseConnection.java
@@ -1,11 +1,12 @@
 package org.example.testejavafxmaven;
 
-import java.sql.Connection;
-
 public class TestDatabaseConnection {
     public static void main(String[] args) {
-        Connection conn = DataBaseConnection.getConnection();
-        if (conn != null) {
+        // Inicializa o esquema do banco de dados (cria as tabelas)
+        DataBaseConnection.inicializarSchema();
+
+        // Testa a conexão com o banco de dados
+        if (DataBaseConnection.getConnection() != null) {
             System.out.println("Conexão com a base de dados testada com sucesso!");
         }
     }
